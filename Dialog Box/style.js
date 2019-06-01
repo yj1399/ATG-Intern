0151000877f6f43c4b9d56751243bdf1f3fe3e8c
Yash Raj Singh <40159239+yj1399@users.noreply.github.com>
2019-06-02 01:54:35 +0530

    Create style.js

---
+++ Dialog Box/style.js
@@ -0,0 +1,8 @@
+$(function(){
+
+    $(".dropdown-menu").on('click', 'li a', function(){
+      $(".btn:first-child").text($(this).text());
+      $(".btn:first-child").val($(this).text());
+   });
+
+});
